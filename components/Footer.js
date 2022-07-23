03d14e01760ce9124d570d3d5e4827a6d2b458c5
eathren <nolan.braman@gmail.com>
2022-07-22 22:13:01 -0800

    placeholder update

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,6 @@
 import SocialIcon from '@/components/social-icons'
 import siteMetadata from '@/data/siteMetadata'
-import Link from './Link'
-
+import Link from 'next/link'
 export default function Footer() {
   return (
     <footer>
@@ -15,16 +14,11 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
+          <Link href="/">{siteMetadata.author}</Link>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          {/* <div>{` • `}</div> */}
+          {/* <Link href="/">{siteMetadata.title}</Link> */}
         </div>
       </div>
     </footer>
