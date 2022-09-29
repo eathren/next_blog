ff45368114d8d77800879d9d2dcb2b0d616bd692
Nolan Braman <nolan.braman@gmail.com>
2022-09-28 20:55:11 -0700

    changes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,7 @@ const siteMetadata = {
   description: 'Blog and thoughts about the state of software in the US',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://nolanbraman.com',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
